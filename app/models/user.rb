7367965f1dd4bd3ff8b62cf455c516d966fb0987
DESKTOP-Q75A6S2\USER <anhhuynh98@gmail.com>
2017-01-20 20:12:56 +0700

    User + CRUD tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :task, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 end
